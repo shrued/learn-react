1369c97cb3546c030c5dc1c952cfabd82ad050fe
Shruti <52789421+shrued@users.noreply.github.com>
2024-02-10 12:56:43 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,23 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+import Profile_Mistake from './qcomps/profile_mistake'
+import MyComp from './qcomps/firstcomp'
+import Bio from './qcomps/bios'
+import TodoList from './qcomps/todos'
+import Gallery from './qcomps/gallery_props'
+import PackingList from './qcomps/props_item'
 
 export default function Home() {
   return (
     <div className={styles.main}>
         <Profile />
+        <Profile_Mistake/>
+        <MyComp/>
+        <Bio/>
+        <TodoList/>
+        <Gallery/>
+        <PackingList/>
     </div>
   )
 }
