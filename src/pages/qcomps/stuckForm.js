e2437072798e32e6e3de17174afd452f54fca705
Shruti <52789421+shrued@users.noreply.github.com>
2024-02-19 22:57:08 -0500

    Update stuckForm.js

--- src/pages/qcomps/stuckForm.js
+++ src/pages/qcomps/stuckForm.js
@@ -1,36 +1,36 @@
 import { useState } from 'react';
 
 export default function Form() {
-  let firstName = '';
-  let lastName = '';
+    const [firstName, setFirstName] = useState(''); 
+    const [lastName, setLastName] = useState(''); 
 
-  function handleFirstNameChange(e) {
-    firstName = e.target.value;
-  }
+    function handleFirstNameChange(e) {
+        setFirstName(e.target.value); 
+    }
 
-  function handleLastNameChange(e) {
-    lastName = e.target.value;
-  }
+    function handleLastNameChange(e) {
+        setLastName(e.target.value); 
+    }
 
-  function handleReset() {
-    firstName = '';
-    lastName = '';
-  }
+    function handleReset() {
+        setFirstName(''); 
+        setLastName(''); 
+    }
 
-  return (
-    <form onSubmit={e => e.preventDefault()}>
-      <input
-        placeholder="First name"
-        value={firstName}
-        onChange={handleFirstNameChange}
-      />
-      <input
-        placeholder="Last name"
-        value={lastName}
-        onChange={handleLastNameChange}
-      />
-      <h1>Hi, {firstName} {lastName}</h1>
-      <button onClick={handleReset}>Reset</button>
-    </form>
-  );
+    return (
+        <form onSubmit={e => e.preventDefault()}>
+            <input
+                placeholder="First name"
+                value={firstName}
+                onChange={handleFirstNameChange}
+            />
+            <input
+                placeholder="Last name"
+                value={lastName}
+                onChange={handleLastNameChange}
+            />
+            <h1>Hi, {firstName} {lastName}</h1>
+            <button type="button" onClick={handleReset}>Reset</button>
+        </form>
+    );
 }
